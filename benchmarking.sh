f0ff43b1eb2b436049b448cd9a4c1c055624790d
Leon Debnath <10490509+S010MON@users.noreply.github.com>
2021-11-25 11:37:25 +0100

    Update and rename Benchmarking.txt to benchmarking.sh

---
+++ benchmarking.sh
@@ -0,0 +1,18 @@
+
+# Terminal Interface
+sudo apt install lm-sensors
+sudo sensors-detect
+sensors
+
+
+# Graphical Temp Tracking
+sudo apt install psensor
+
+# Graphical Sys Info/Benchmarking
+sudo apt install hardinfo
+
+# Sysbench
+sudo apt install sysbench
+sysbench cpu run
+sysbench memory run
+sysbench --help
